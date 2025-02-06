fd1d82cd7b12fef3bd9768bbcc462427a7bbb09c
julioc <julioc@alexsa.com.py>
2025-02-06 15:57:17 -0300

    Clase 2 Toolbar and tabRow

--- app/src/main/java/py/com/alexsa/movies/MainActivity.kt
+++ app/src/main/java/py/com/alexsa/movies/MainActivity.kt
@@ -4,13 +4,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.Black
+import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import py.com.alexsa.movies.ui.theme.MoviesTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,28 +41,68 @@ class MainActivity : ComponentActivity() {
         setContent {
             MoviesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    val aa = innerPadding
+                    content()
                 }
             }
         }
     }
 }
-
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun content() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp)
+            .background(Black)
+    ) {
+        Column(Modifier.fillMaxWidth()) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Black)
+                    .padding(horizontal = 10.dp, vertical = 20.dp),
+                contentAlignment = Alignment.CenterStart
+            ) {
+                Icon(
+                    Icons.Filled.Menu,
+                    contentDescription = "Menu",
+                    tint = White
+                )
+                Text(
+                    text = "Movies",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth(),
+                    color = White,
+                    fontSize = 20.sp,
+                    style = TextStyle(fontWeight = FontWeight.Bold)
+                )
+            }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MoviesTheme {
-        Greeting("Android")
+            val tabs = listOf("Popular", "In Theaters", "Coming Soon")
+            var selectedTabIndex by remember { mutableStateOf(0) }
+
+            TabRow(
+                selectedTabIndex = selectedTabIndex,
+                containerColor = Black,
+                contentColor = White,
+                modifier = Modifier.fillMaxWidth()
+            ){
+                tabs.forEachIndexed { index, title ->
+                    Tab(
+                        selected = selectedTabIndex == index,
+                        onClick = {selectedTabIndex = index},
+                        text = {
+                            Text(
+                                text = title,
+                                color = if(selectedTabIndex==index) Red else White,
+                                style = TextStyle(fontWeight = FontWeight.Bold)
+                            )
+                        }
+                    )
+                }
+            }
+        }
     }
 }
